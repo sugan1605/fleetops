f5fdc5498d274ecd87a449516983b212aadf44ba
Losugan Sivasuthan <ssv1605@outlook.com>
2026-09-11 16:24:50 +0200

    feat: add reservation behavior and test fixtures

--- app/reservation.py
+++ app/reservation.py
@@ -8,9 +8,17 @@ class Reservation:
     def __init__(
         self, customer: Customer, vehicle: Vehicle, start: datetime, end: datetime
     ):
+        if end < start:
+            raise ValueError("end date can't be earlier than start date.")
+        
         self.customer = customer
         self.vehicle = vehicle
         self.start = start
         self.end = end
 
+    def is_active(self, current_time: datetime) -> bool:
+        return self.start <= current_time <= self.end
 
+
+
+    
--- tests/test_reservation.py
+++ tests/test_reservation.py
@@ -1,33 +1,64 @@
 from datetime import datetime
 
+import pytest
+
 from app.customer import Customer
 from app.reservation import Reservation
 from app.vehicle import Vehicle
 
 
-def test_reservation_creation():
+
+# Test fixtures
+@pytest.fixture
+def test_customer():
     customer = Customer(
-        customer_id=545,
+        customer_id=5656,
         first_name="Losugan",
         last_name="Sivasuthan",
+        email="ssv1605@test.com",
         phone_number="+47 21390016",
-        email="test1@test.no",
+
     )
 
+    return customer
+
+@pytest.fixture
+def test_vehicle():
     vehicle = Vehicle(
-        registration_number="DS 35637",
-        make="Volvo",
-        model="XC-60",
-        fuel_type="Petrol",
-        operational_status="AVAILABLE",
-        odometer_km=7500,
-    )
+     registration_number="DS 35637",
+     make="Volvo",
+     model="XC-60",
+     fuel_type="Petrol",
+     operational_status="AVAILABLE",
+     odometer_km=7500,
+
+     )
+
+    return vehicle
+
+def test_reservation_creation(test_customer, test_vehicle):
+    
+
+   
+
+    start = datetime(2026, 9, 25, 17, 0)
+    end = datetime(2026, 9, 20, 17, 0)
 
     # create reservation
+    with pytest.raises(ValueError):
+        Reservation(
+            customer=test_customer,
+            vehicle=test_vehicle,
+            start=start,
+            end=end,
+        )
 
-    reservation = Reservation(
-        customer=customer,
-        vehicle=vehicle,
-        start=datetime(2026, 9, 25, 17, 0),
-        end=datetime(2026, 9, 30, 17, 0),
-    )
+
+def test_reservation_is_active(test_customer, test_vehicle):
+
+    start = datetime(2026, 9, 25, 17, 0)
+    end = datetime(2026, 9, 30, 17, 0)
+
+    reservation = Reservation(customer=test_customer, vehicle=test_vehicle, start=start, end=end)
+
+    assert reservation.is_active(datetime(2026, 9, 27, 12, 0))
